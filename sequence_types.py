ff951d46e9f4bf13251424903bc3514c83fbdd42
Bhupendrajeet Singh <bhupendrajeet.singh@metacube.com>
2025-01-08 12:11:54 +0530

    sequence types assignment

---
+++ sequence_types.py
@@ -0,0 +1,10 @@
+countries = ["India", "Australia", "Russia"]
+countries.append("China")
+countries.pop(3)
+countries.insert(2, "England")
+
+
+countries = {"India", "England", "Australia"}
+countries.add("China")
+
+#Not possible to add or remove an element on the basis of index in sets
